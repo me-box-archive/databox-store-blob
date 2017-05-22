335a775bf0ca87232e8133c8592c12c2f6d1a87e
ToshBrown <Toshbrown@gmail.com>
2017-05-22 22:32:39 +0100

    Fix tests

--- src/lib/databox-request/databox-request-promise.js
+++ src/lib/databox-request/databox-request-promise.js
@@ -9,8 +9,12 @@ const fs = require('fs');
 //Databox ENV vars
 //
 const DATABOX_ARBITER_ENDPOINT = process.env.DATABOX_ARBITER_ENDPOINT || "https://databox-arbiter:8080";
-const ARBITER_TOKEN = fs.readFileSync("/run/secrets/ARBITER_TOKEN",{encoding:'base64'});
-
+let ARBITER_TOKEN = '';
+try {
+    ARBITER_TOKEN = fs.readFileSync("/run/secrets/ARBITER_TOKEN",{encoding:'base64'});
+} catch (e) {
+    ARBITER_TOKEN = '';
+}
 /**
  * This module wraps the node request module https://github.com/request/request and adds:
  * 
